11dca1e85ce944fdf50352d8b54f04b5a2186c94
nbarban <nbarban@millhouse.com>
2021-06-24 10:29:43 +0300

    Initial commit

---
+++ src/main/java/com/nb/news/bessarabianinform/BessarabianinformApplication.java
@@ -0,0 +1,13 @@
+package com.nb.news.bessarabianinform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BessarabianinformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BessarabianinformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nb/news/bessarabianinform/BessarabianinformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.news.bessarabianinform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BessarabianinformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
