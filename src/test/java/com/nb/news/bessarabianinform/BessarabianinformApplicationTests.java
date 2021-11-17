b6c7ad19e6cb09b92aae1085c5433128c4eeba98
nbarban <nbarban@millhouse.com>
2021-11-17 19:33:32 +0200

    clean up and fix test

--- src/test/java/com/nb/news/bessarabianinform/BessarabianinformApplicationTests.java
+++ src/test/java/com/nb/news/bessarabianinform/BessarabianinformApplicationTests.java
@@ -6,10 +6,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 
 @SpringBootTest
+@TestPropertySource(properties = "PORT=8080")
 class BessarabianinformApplicationTests {
 
     @Autowired
