ea63ba37f5b470b01911aa2c5ea397faa7f6ddbe
Anatolii Mitiliuk <anatoliimitiliuk@gmail.com>
2022-02-03 15:32:26 +0200

    remove this GorodApplication remove Test

--- src/test/java/com/nb/news/bessarabianinform/BessarabianinformApplicationTests.java
+++ src/test/java/com/nb/news/bessarabianinform/BessarabianinformApplicationTests.java
@@ -2,6 +2,7 @@ package com.nb.news.bessarabianinform;
 
 import com.nb.news.bessarabianinform.domain.Article;
 import com.nb.news.bessarabianinform.service.NewsParser;
+import com.nb.news.bessarabianinform.service.NewsSecondParser;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,11 @@ class BessarabianinformApplicationTests {
     @Autowired
     private NewsParser sut;
 
+ @Autowired
+    private NewsSecondParser suts;
+
+
+
     @Test
     void should_get_news() {
         List<Article> articles = sut.getNews();
@@ -26,4 +32,15 @@ class BessarabianinformApplicationTests {
             return true;
         });
     }
+
+    @Test
+    void should_get_news_second() {
+        List<Article> articles = suts.getNews2();
+
+        org.assertj.core.api.Assertions.assertThat(articles).isNotEmpty().doesNotContainNull().allMatch(article -> {
+            Assertions.assertThat(article).hasNoNullFieldsOrProperties();
+            return true;
+        });
+    }
+
 }
